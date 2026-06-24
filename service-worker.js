a0ea855092a2b269b8c27a9fbc0f7c80e0a28941
Messenger-Mas <andreymomot007@gmail.com>
2026-06-24 10:35:23 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,23 +2,44 @@ const CACHE_NAME = 'boom-brawl-v1';
 const ASSETS = [
   '/Boom-Brawl/',
   '/Boom-Brawl/index.html',
-  '/Boom-Brawl/manifest.json'
+  '/Boom-Brawl/manifest.json',
+  '/Boom-Brawl/icon-192x192.png',
+  '/Boom-Brawl/icon-512x512.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-    if(resp && resp.status === 200) {
-      const clone = resp.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-    }
-    return resp;
-  }).catch(() => caches.match('/index.html'))));
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request)
+        .then(resp => {
+          if (resp && resp.status === 200 && resp.type === 'basic') {
+            const clone = resp.clone();
+            caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+          }
+          return resp;
+        })
+        .catch(() => {
+          if (e.request.mode === 'navigate') {
+            return caches.match('/Boom-Brawl/');
+          }
+          return null;
+        })
+      )
+  );
 });
