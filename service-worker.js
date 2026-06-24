acab9931ecea38f07fe9d3c9c960c40d1970952a
Messenger-Mas <andreymomot007@gmail.com>
2026-06-24 10:06:47 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'boom-brawl-v1';
+const ASSETS = ['/','/index.html','/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+    if(resp && resp.status === 200) {
+      const clone = resp.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+    }
+    return resp;
+  }).catch(() => caches.match('/index.html'))));
+});
